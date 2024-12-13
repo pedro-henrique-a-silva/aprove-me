11f0f19ce72ed1675f35f19d5e97c552c853d3b5
Pedro Henrique <pedro.henrique.a.s66@gmail.com>
2024-12-13 18:56:27 -0300

    feat: implementa envio de e-mails de sucesso e falha de pagamento com tratamento de exceções

--- bankme/src/integrations/email/email.service.ts
+++ bankme/src/integrations/email/email.service.ts
@@ -1,16 +1,53 @@
-import { Injectable } from '@nestjs/common';
-import { Resend } from 'resend';
+import { Injectable, ServiceUnavailableException } from '@nestjs/common';
+import { MailerService } from '@nestjs-modules/mailer';
 
 @Injectable()
 export class EmailService {
-  sendEmail(email: string, subject: string, message: string) {
-    const resend = new Resend(process.env.RESEND_API_KEY);
+  constructor(private readonly mailerService: MailerService) {}
 
-    resend.emails.send({
-      from: 'onboarding@resend.dev',
-      to: email,
-      subject: subject,
-      html: message,
-    });
+  async sendSuccessPaymentEmail(
+    email: string,
+    name: string,
+    value: number,
+  ): Promise<void> {
+    try {
+      await this.mailerService.sendMail({
+        to: process.env.TO,
+        subject: 'Pagamento Processado com sucesso',
+        template: 'success-payment',
+        context: {
+          name,
+          value,
+        },
+      });
+    } catch (error) {
+      console.error('Error sending email:', error);
+      throw new ServiceUnavailableException(
+        'Email service is temporarily unavailable. Please try again later.',
+      );
+    }
+  }
+
+  async sendFailurePaymentEmail(
+    email: string,
+    name: string,
+    value: number,
+  ): Promise<void> {
+    try {
+      await this.mailerService.sendMail({
+        to: process.env.TO,
+        subject: 'O Processamento do seu pagmento falhou',
+        template: 'failed-payment',
+        context: {
+          name,
+          value,
+        },
+      });
+    } catch (error) {
+      console.error('Error sending email:', error);
+      throw new ServiceUnavailableException(
+        'Email service is temporarily unavailable. Please try again later.',
+      );
+    }
   }
 }
