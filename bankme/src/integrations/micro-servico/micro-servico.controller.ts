019e6f1d8c79e618e9b05ed754330d810ec6d5b4
Pedro Henrique <pedro.henrique.a.s66@gmail.com>
2024-12-13 18:56:49 -0300

    feat: altera controlador para usar MessagePattern e processar mensagens da fila de pagamentos

--- bankme/src/integrations/micro-servico/micro-servico.controller.ts
+++ bankme/src/integrations/micro-servico/micro-servico.controller.ts
@@ -1,15 +1,18 @@
 import { MicroServicoService } from './micro-servico-consumer.service';
-import { EventPattern, Payload } from '@nestjs/microservices';
+import { Ctx, MessagePattern, RmqContext } from '@nestjs/microservices';
 import { Controller } from '@nestjs/common';
-import PayableCreationDto from '../payable/dto/PayableCreationDto';
 
-@Controller('microserviço')
-export class PayableController {
+@Controller()
+export class MicroServicoController {
   constructor(private readonly payableService: MicroServicoService) {}
 
-  @EventPattern('payable_batch')
-  async processPayable(@Payload() payload: PayableCreationDto[]) {
-    await this.payableService.processPayable(payload);
+  @MessagePattern('payable_queue')
+  async processPayable(@Ctx() context: RmqContext) {
+    const message = context.getMessage();
+    const content = JSON.parse(message.content);
+
+    const { payables } = content;
+    await this.payableService.processPayable(payables);
     return;
   }
 }
