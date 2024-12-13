89431fd972e4693ed22d91168ad367161f504c85
Pedro Henrique <pedro.henrique.a.s66@gmail.com>
2024-12-13 18:56:09 -0300

    feat: adiciona decorator Public ao método de login no AuthController

--- bankme/src/integrations/auth/auth.controller.ts
+++ bankme/src/integrations/auth/auth.controller.ts
@@ -2,6 +2,7 @@ import { Body, Controller, HttpCode, Post } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import AuthLoginDto from './dto/AuthLoginDto';
 import { ApiBody, ApiResponse, ApiTags } from '@nestjs/swagger';
+import { Public } from '../shared/custom-decorators/is-public.decorator';
 
 @Controller('auth')
 @ApiTags('Authentication')
@@ -13,6 +14,7 @@ export class AuthController {
   }
 
   @Post('login')
+  @Public()
   @HttpCode(200)
   @ApiResponse({
     status: 200,
