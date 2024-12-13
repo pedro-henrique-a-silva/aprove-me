6fcb1bd9e50fd4688a45b0f19cd161a48965efb5
Pedro Henrique <pedro.henrique.a.s66@gmail.com>
2024-12-13 19:04:57 -0300

    feat: integra MailerModule e configura AuthGuard no AppModule

--- bankme/src/app.module.ts
+++ bankme/src/app.module.ts
@@ -6,17 +6,29 @@ import { AuthModule } from './integrations/auth/auth.module';
 import { JwtModule } from '@nestjs/jwt';
 import { ClientsModule, Transport } from '@nestjs/microservices';
 import { MicroServicoModule } from './integrations/micro-servico/micro-servico.module';
+import { APP_GUARD } from '@nestjs/core';
+import { AuthGuard } from './integrations/shared/guards/auth.guard';
+import { MailerModule } from '@nestjs-modules/mailer';
+import { HandlebarsAdapter } from '@nestjs-modules/mailer/dist/adapters/handlebars.adapter';
+import { join } from 'path';
+
+const isDevelopment = process.env.NODE_ENV !== 'production';
+const templatePath = isDevelopment
+  ? join(__dirname, '../../', 'src/templates')
+  : join(__dirname, '../../', 'src/templates');
 
 @Module({
   imports: [
     AuthModule,
     AssignorModule,
     PayableModule,
-    MicroServicoModule,
     JwtModule.register({
       global: true,
       secret: process.env.JWT_SECRET || 'secret',
-      signOptions: { expiresIn: '1d', algorithm: 'HS256' },
+      signOptions: {
+        expiresIn: `${process.env.JWT_EXPIRES_IN}`,
+        algorithm: 'HS256',
+      },
     }),
     ClientsModule.register([
       {
@@ -32,7 +44,35 @@ import { MicroServicoModule } from './integrations/micro-servico/micro-servico.m
         },
       },
     ]),
+    MicroServicoModule,
+    MailerModule.forRoot({
+      transport: {
+        host: process.env.SMTP_HOST,
+        port: process.env.SMTP_PORT,
+        secure: false,
+        auth: {
+          user: process.env.SMTP_USERNAME,
+          pass: process.env.SMTP_PASSWORD,
+        },
+      },
+      defaults: {
+        from: process.env.SMTP_FROM,
+      },
+      template: {
+        dir: join(templatePath),
+        adapter: new HandlebarsAdapter(),
+        options: {
+          strict: true,
+        },
+      },
+    }),
   ],
-  providers: [PrismaService],
+  providers: [
+    PrismaService,
+    {
+      provide: APP_GUARD,
+      useClass: AuthGuard,
+    },
+  ],
 })
 export class AppModule {}
