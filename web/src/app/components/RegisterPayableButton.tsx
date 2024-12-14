95efcc88c381108462283e3eb193e03d6f66cf1b
Pedro Henrique <pedro.henrique.a.s66@gmail.com>
2024-12-14 20:38:35 -0300

    feat: adiciona botão para registrar recebível

---
+++ web/src/app/components/RegisterPayableButton.tsx
@@ -0,0 +1,21 @@
+"use client"
+import { useRouter } from 'next/navigation';
+import React from 'react'
+
+function RegisterPayableButton() {
+  const router = useRouter();
+
+  const handleClick = () => {
+    router.push('/payable/create');
+  }
+  return (
+    <button
+      onClick={handleClick}
+      className="px-4 py-1 w-1/2 mb-6 rounded-md bg-sky-500 hover:bg-sky-700 hover:text-cyan-50" 
+    >
+          Registrar Recebivel
+    </button>
+  )
+}
+
+export default RegisterPayableButton
