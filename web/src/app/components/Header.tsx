249f5d6d522d5b3e6c212c367ea60bc25fe4ae6b
Pedro Henrique <pedro.henrique.a.s66@gmail.com>
2024-12-14 15:57:36 -0300

    feat: adiciona componente Header com layout básico

---
+++ web/src/app/components/Header.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Header() {
+  return (
+    <div className='mx-auto flex justify-between bg-gray-400 p-6'>
+      <h1>Logo</h1>
+      <div>Menu</div>
+    </div>
+  )
+}
+
+export default Header
