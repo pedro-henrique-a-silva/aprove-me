b3c2701bd2a853f9cb6dc548380ba3c13f6d7986
Pedro Henrique <pedro.henrique.a.s66@gmail.com>
2024-12-14 18:05:07 -0300

    feat: adiciona gerenciamento de erros e redirecionamento no componente Signup

--- web/src/app/components/Signup.tsx
+++ web/src/app/components/Signup.tsx
@@ -1,10 +1,17 @@
 "use client"
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import { signupSchema } from '../schemas/signup-schema';
 import { ZodError } from 'zod';
+import { connection } from '../utils/api-connection';
+import { useRouter } from 'next/navigation';
 
-function Signup() {
-  const [error, setError] = useState<string[]>([]);
+type SignupProps = {
+  setIsLogin: (isLogin: boolean) => void;
+}
+
+function Signup(props: SignupProps) {
+  const {  setIsLogin } = props;
+  const [error, setError] = useState<string[] | null>(null);
 
   async function handleSubmit (e: React.FormEvent<HTMLFormElement>) {
     e.preventDefault();
@@ -16,11 +23,38 @@ function Signup() {
       setError(JSON.parse(error.message).map((e: ZodError) => e.message));
       return;
     }
-  }
 
+    try {
+      await connection.post('/integrations/assignor', {
+        ...data,
+        name: data.nome,
+        document: data.cpf,
+      })
+
+      setTimeout(() => {
+        setIsLogin(true);
+      }, 1500);
+
+    } catch (error: any) {
+      if (error.response.status !== 201) {
+        setError(['Error creating new assignor']);
+        return;
+      }
+    }
+    
+  }
+  
+  useEffect(() => {
+    if (error) {
+      setTimeout(() => {
+        setError(null);
+      }, 3000);
+    }
+  }, [error])
 
   return (
-    <form onSubmit={handleSubmit} className='flex flex-col justify-center mt-10'>
+    <>
+    <form onSubmit={handleSubmit} className='flex flex-col justify-center w-full mt-10'>
         <div className='flex flex-col pb-1'>
           <label htmlFor="">Nome</label>
           <input className='border shadow' id='nome' name='nome' type="text" />
@@ -45,14 +79,15 @@ function Signup() {
           <button className="px-4 py-1 rounded-md bg-sky-500 hover:bg-sky-700 hover:text-cyan-50" type="submit">Sign up</button>
         </div>
 
-        {error.length && (
-          <div className='flex flex-col justify-center'>
-            {error.map((e, i) => (
-              <div key={i} className='text-red-400 bg-red-100 mb-2 p-2 rounded text-center'>{e}</div>
-            ))}
-          </div>
-        )}
     </form>
+    {error && (
+      <div className='flex flex-col justify-center'>
+        {error.map((e, i) => (
+          <div key={i} className='text-red-400 bg-red-100 mb-2 p-2 rounded text-center'>{e}</div>
+        ))}
+      </div>
+    )}
+    </>
   )
 }
 
