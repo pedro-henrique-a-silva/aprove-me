722e4ef476fb3d4513d80b9d8f0810ae92157d7e
Pedro Henrique <pedro.henrique.a.s66@gmail.com>
2024-12-14 20:52:23 -0300

    feat: atualiza dados do cedente no seeder, alterando nome, email e documento

--- bankme/prisma/seeders/assignor.seed.ts
+++ bankme/prisma/seeders/assignor.seed.ts
@@ -8,12 +8,10 @@ export default async function assignorSeed() {
     data: [
       {
         id: '9fe7de00-6f6c-45e2-8dfe-737f945614ef',
-        name: 'Fulano',
-        email: 'email@email.com',
-        password: await bcrypt.hash('123456', 10),
-        document: 'sdfadsfasd',
+        name: 'John',
+        email: 'john@doe.com',
+        document: '00000000000',
         phone: '99999999999',
-        // password: '123456',
       },
     ],
   });
