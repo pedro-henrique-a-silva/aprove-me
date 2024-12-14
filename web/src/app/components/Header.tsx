e3503b4c453b71466852b7e32471492509aaa550
Pedro Henrique <pedro.henrique.a.s66@gmail.com>
2024-12-14 20:37:47 -0300

    feat: atualiza cabeçalho para incluir links de navegação

--- web/src/app/components/Header.tsx
+++ web/src/app/components/Header.tsx
@@ -1,10 +1,16 @@
+import Link from 'next/link'
 import React from 'react'
 
 function Header() {
   return (
     <div className='mx-auto flex justify-between bg-gray-400 p-6'>
       <h1>Logo</h1>
-      <div>Menu</div>
+      <div className='flex justify-center items-center gap-2'>
+        <Link href={'/'}>Home</Link>
+        <Link href={'/payable'}>Recebiveis</Link>
+        <Link href={'/assignor'}>Cedente</Link>
+
+      </div>
     </div>
   )
 }
