5349e9b7b464d901da9c6e5844b51824bddfbe4e
Pedro Henrique <pedro.henrique.a.s66@gmail.com>
2024-12-14 20:36:45 -0300

    feat: adiciona página de cedentes com botão de registro e lista de cedentes

---
+++ web/src/app/assignor/page.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import ListAssignor from '../components/ListAssignors'
+import RegisterCedenteButton from '../components/RegisterAssignorButton'
+
+function page() {
+  return (
+    <>
+    <div className='p-6 flex justify-center items-center flex-col'>
+      <RegisterCedenteButton/>
+      <ListAssignor />
+    </div>
+  </>
+  )
+}
+
+export default page
