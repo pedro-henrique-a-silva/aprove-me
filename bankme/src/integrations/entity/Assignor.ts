b59da410718cab35b53b08039b93243ca1e44443
Pedro Henrique <pedro.henrique.a.s66@gmail.com>
2024-12-14 20:35:20 -0300

    feat: remove campo de senha da entidade Assignor e seus métodos associados

--- bankme/src/integrations/entity/Assignor.ts
+++ bankme/src/integrations/entity/Assignor.ts
@@ -2,7 +2,6 @@ export default class Assignor {
   private _id: string;
   private _document: string;
   private _email: string;
-  private _password: string;
   private _phone: string;
   private _name: string;
   private _active: boolean;
@@ -11,7 +10,6 @@ export default class Assignor {
     id?: string,
     document?: string,
     email?: string,
-    password?: string,
     phone?: string,
     name?: string,
     active?: boolean,
@@ -19,7 +17,6 @@ export default class Assignor {
     this.id = id;
     this.document = document;
     this.email = email;
-    this.password = password;
     this.phone = phone;
     this.name = name;
     this.active = active;
@@ -47,14 +44,6 @@ export default class Assignor {
 
   set email(email: string) {
     this._email = email;
-  }
-
-  get password(): string {
-    return this._password;
-  }
-
-  set password(password: string) {
-    this._password = password;
   }
 
   get phone(): string {
@@ -85,7 +74,6 @@ export default class Assignor {
     return {
       document: this.document,
       email: this.email,
-      password: this.password,
       phone: this.phone,
       name: this.name,
       id: this.id,
