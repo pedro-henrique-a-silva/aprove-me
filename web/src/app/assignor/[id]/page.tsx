fbb5edcc4b17270ebdc71a1e0789facc33e587a1
Pedro Henrique <pedro.henrique.a.s66@gmail.com>
2024-12-14 20:36:53 -0300

    feat: adiciona página de detalhes do cedente

---
+++ web/src/app/assignor/[id]/page.tsx
@@ -0,0 +1,12 @@
+import AssignorDetail from '@/app/components/AssignorDetail'
+import React from 'react'
+
+function page() {
+  return (
+    <div>
+      <AssignorDetail />
+    </div>
+  )
+}
+
+export default page
