2e65b1823a5107bc8d324390481b9491102cfc21
Pedro Henrique <pedro.henrique.a.s66@gmail.com>
2024-12-14 20:39:41 -0300

    feat: adiciona função para formatar data no formato da API

--- web/src/app/utils/date-helper.ts
+++ web/src/app/utils/date-helper.ts
@@ -3,3 +3,7 @@ import moment from 'moment'
 export function formatDate(date: string): string {
   return moment(date).format('DD/MM/YYYY')
 }
+
+export function formatDateToApiFormat(date: string): string {
+  return moment(date).format('YYYY-MM-DD')
+}
