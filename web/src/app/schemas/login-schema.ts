6b8b6a333cc98f13e759e89366b752a3d0bbb007
Pedro Henrique <pedro.henrique.a.s66@gmail.com>
2024-12-14 20:39:25 -0300

    feat: altera esquema de validação para login, substituindo email por username

--- web/src/app/schemas/login-schema.ts
+++ web/src/app/schemas/login-schema.ts
@@ -1,9 +1,8 @@
 import { z } from "zod";
 
 export const loginSchema = z.object({
-  email: z
-    .string({ message: "O e-mail deve ser válido." })
-    .email({ message: "O e-mail deve ser válido." }),
+  username: z
+    .string({ message: "O username deve ser válido." }),
   password: z
     .string({ message: "A senha deve ter pelo menos 6 caracteres." })
     .min(6, { message: "A senha deve ter pelo menos 6 caracteres." }),
