f1f8876eaae47d2923195d03f2b0a9494174cfaf
Pedro Henrique <pedro.henrique.a.s66@gmail.com>
2024-12-14 20:39:16 -0300

    feat: adiciona esquema de validação para criação de recebíveis

---
+++ web/src/app/schemas/create-payable.ts
@@ -0,0 +1,14 @@
+import { z } from "zod";
+
+export const createPayableSchema = z.object({
+  value: z
+    .string({ message: "O valor não pode ser vazio." }),
+    emissionDate: z
+    .string({ message: "A data de Emissão não pode ser vazia." })
+    .regex(/^\d{2}\/\d{2}\/\d{4}$/, {
+      message: 'Data de emissão Inválida',
+    }),
+    assignors: z.string({ message: "O cedente não pode ser vazio." }).uuid(),
+});
+
+export type CreatePayableData = z.infer<typeof createPayableSchema>;
