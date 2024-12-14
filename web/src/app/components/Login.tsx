c02d86c854f407c883b0ff18ccdaeb0cea290c97
Pedro Henrique <pedro.henrique.a.s66@gmail.com>
2024-12-14 18:04:36 -0300

    feat: adiciona componente Login com validação e gerenciamento de erros

---
+++ web/src/app/components/Login.tsx
@@ -0,0 +1,82 @@
+"use client"
+import React, { useEffect, useState } from 'react'
+import { ZodError } from 'zod';
+import { loginSchema } from '../schemas/login-schema';
+import { connection } from '../utils/api-connection';
+import { useRouter } from 'next/navigation';
+import { saveTokenToLocalStore } from '../utils/local-store-helper';
+
+function Login() {
+  const [error, setError] = useState<string[] | null>(null);
+  const router = useRouter();
+
+  async function handleSubmit (e: React.FormEvent<HTMLFormElement>) {
+    e.preventDefault();
+    const formData = new FormData(e.currentTarget);
+    const data = Object.fromEntries(formData.entries());
+
+
+    const {success, error} = loginSchema.safeParse(data);
+    if (!success) {
+      setError(JSON.parse(error.message).map((e: ZodError) => e.message));
+      return;
+    }
+
+    try {
+      const response = await connection.post('/auth/login', {
+        ...data,
+      })
+
+      const { token } = response.data;
+
+      saveTokenToLocalStore('token', token);
+
+      setTimeout(() => {
+        router.push("/payable");
+      }, 1500);
+
+    } catch (error: any) {
+      if (error.response.status !== 200) {
+        setError(['Error on login']);
+        return;
+      }
+    }
+  }
+
+  useEffect(() => {
+    if (error) {
+      setTimeout(() => {
+        setError(null);
+      }, 3000);
+    }
+  }, [error])
+
+  return (
+    <>
+    <form onSubmit={handleSubmit} className='flex flex-col justify-center w-full mt-10'>
+        <div className='flex flex-col pb-1'>
+          <label htmlFor="">Email</label>
+          <input className='border shadow' id='email' name='email' type="text" />
+        </div>
+        <div className='flex flex-col pb-1'>
+          <label htmlFor="">Password</label>
+          <input className='border shadow' id='password' name='password' type="password" />
+        </div>
+        <div className='flex flex-col justify-center pb-1 mt-2 mb-2'>
+          <button 
+          className="px-4 py-1 rounded-md bg-sky-500 hover:bg-sky-700 hover:text-cyan-50" 
+          type="submit">Login</button>
+        </div>
+    </form>
+    {error && (
+      <div className='flex flex-col justify-center'>
+        {error.map((e, i) => (
+          <div key={i} className='text-red-400 bg-red-100 mb-2 p-2 rounded text-center'>{e}</div>
+        ))}
+      </div>
+    )}
+    </>
+  )
+}
+
+export default Login
