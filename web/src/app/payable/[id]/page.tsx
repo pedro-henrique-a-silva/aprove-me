646385c1f9f464ed811f062dc0c0b6ca0757eb0d
Pedro Henrique <pedro.henrique.a.s66@gmail.com>
2024-12-14 20:38:59 -0300

    feat: adiciona página de detalhes do recebível

---
+++ web/src/app/payable/[id]/page.tsx
@@ -0,0 +1,12 @@
+import PayableDetail from '@/app/components/PayableDetail'
+import React from 'react'
+
+function page() {
+  return (
+    <div>
+      <PayableDetail/>
+    </div>
+  )
+}
+
+export default page
