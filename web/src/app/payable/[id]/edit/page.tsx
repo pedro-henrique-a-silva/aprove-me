418e3c2fbe112512c6c984e8dfde7668d8296daf
Pedro Henrique <pedro.henrique.a.s66@gmail.com>
2024-12-14 21:44:58 -0300

    feat: adiciona página de edição para pagamentos utilizando o componente CreatePayable

---
+++ web/src/app/payable/[id]/edit/page.tsx
@@ -0,0 +1,14 @@
+import CreatePayable from '@/app/components/CreatePayable'
+import React from 'react'
+
+function page() {
+  return (
+    <div className='container w-1/2 mx-auto mt-5 flex justify-center items-center border rounded border-cyan-100 shadow border-solid'>
+      <div className="flex flex-col  w-1/2">
+        <CreatePayable isEditing={true}/>
+      </div>
+    </div>
+  )
+}
+
+export default page
