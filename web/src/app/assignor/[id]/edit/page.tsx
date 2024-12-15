6eb358c2890dce862f0828e3d63b97f45a8f1d35
Pedro Henrique <pedro.henrique.a.s66@gmail.com>
2024-12-14 21:44:19 -0300

    feat: adiciona página de edição para cedentes com componente CreateAssignor

---
+++ web/src/app/assignor/[id]/edit/page.tsx
@@ -0,0 +1,12 @@
+import CreateAssignor from '@/app/components/CreateAssignor'
+import React from 'react'
+
+function page() {
+  return (
+    <div>
+      <CreateAssignor isEditing={true}/>
+    </div>
+  )
+}
+
+export default page
