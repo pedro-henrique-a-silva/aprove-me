fe0ffc308eff1ca6e884cc519f270c5ae4b7d6fe
Pedro Henrique <pedro.henrique.a.s66@gmail.com>
2024-12-14 21:45:14 -0300

    feat: atualiza regex da data de emissão para o formato YYYY-MM-DD no schema de criação de pagamentos

--- web/src/app/schemas/create-payable.ts
+++ web/src/app/schemas/create-payable.ts
@@ -5,7 +5,7 @@ export const createPayableSchema = z.object({
     .string({ message: "O valor não pode ser vazio." }),
     emissionDate: z
     .string({ message: "A data de Emissão não pode ser vazia." })
-    .regex(/^\d{2}\/\d{2}\/\d{4}$/, {
+    .regex(/^\d{4}-\d{2}-\d{2}$/, {
       message: 'Data de emissão Inválida',
     }),
     assignors: z.string({ message: "O cedente não pode ser vazio." }).uuid(),
