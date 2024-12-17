b4fce091c144a842a03c898b2d820625e1c6ec35
Pedro Henrique <pedro.henrique.a.s66@gmail.com>
2024-12-17 20:21:59 -0300

    feat: adiciona nova conexão de servidor com axios

--- web/src/app/utils/api-connection.ts
+++ web/src/app/utils/api-connection.ts
@@ -4,3 +4,7 @@ const BASE_URL = `${process.env.NEXT_PUBLIC_API_URL}/` || "http://localhost:3001
 export const connection = axios.create({
   baseURL: BASE_URL,
 });
+
+export const serverConnection = axios.create({
+  baseURL: 'http://bankme_api:3001/',
+});
