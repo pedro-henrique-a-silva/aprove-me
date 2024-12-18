1104ba7003097a7ea9d253e860de75cebabc7f7b
Pedro Henrique <pedro.henrique.a.s66@gmail.com>
2024-12-18 20:03:24 -0300

    feat: substitui botões padrão por componentes personalizados do shadcn/ui

--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -2,15 +2,30 @@
 import { useState } from "react";
 import Login from "./components/Login";
 import Signup from "./components/Signup";
+import { Button } from "@/components/ui/button";
 
 export default function Home() {
   const [isLogin, setIsLogin] = useState(true);
   return (
-    <div className='container w-1/2 mx-auto mt-5 flex justify-center items-center border rounded border-cyan-100 shadow border-solid'>
-      <div className="flex flex-col  w-1/2">
+    <div className='container w-1/2 mx-auto mt-5 flex justify-center items-center'>
+      <div className="flex flex-col  w-1/2 py-4">
         <div className="flex justify-between bg-slate-50 gap-2">
-          <button className="w-1/2 px-4 py-1 rounded-md bg-sky-500 hover:bg-sky-700 hover:text-cyan-50" onClick={() => setIsLogin(true)}>Login</button>
-          <button className="w-1/2 px-4 py-1 rounded-md bg-sky-500 hover:bg-sky-700 hover:text-cyan-50" onClick={() => setIsLogin(false)}>Sign up</button>
+          <Button 
+            className='bg-blue-700 hover:bg-blue-600 w-1/2'
+            onClick={() => setIsLogin(true)}
+            disabled={isLogin}
+            type="button"
+            >
+              Login
+          </Button>
+          <Button 
+            className='bg-blue-700 hover:bg-blue-600 w-1/2'
+            onClick={() => setIsLogin(false)}
+            disabled={!isLogin}
+            type="button"
+            >
+              Sign up
+          </Button>
         </div>
         {
           isLogin ? <Login /> : <Signup setIsLogin={setIsLogin}/>
