7f2537116b2acc0b87d2a7dcd9c32be45f6009ed
Pedro Henrique <pedro.henrique.a.s66@gmail.com>
2024-12-18 20:05:15 -0300

    feat: substitui formulário de login por um cartão estilizado com componentes do shadcn/ui

--- web/src/app/components/Login.tsx
+++ web/src/app/components/Login.tsx
@@ -6,6 +6,11 @@ import { connection } from '../utils/api-connection';
 import { useRouter } from 'next/navigation';
 import { saveTokenToLocalStore } from '../utils/local-store-helper';
 import { saveCookie } from '../utils/cookies-helper';
+import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from '@/components/ui/card';
+import Image from 'next/image';
+import { Input } from '@/components/ui/input';
+import { Label } from '@/components/ui/label';
+import { Button } from '@/components/ui/button';
 
 
 function Login() {
@@ -56,28 +61,46 @@ function Login() {
 
   return (
     <>
-    <form onSubmit={handleSubmit} className='flex flex-col justify-center w-full mt-10'>
-        <div className='flex flex-col pb-1'>
-          <label htmlFor="">Username</label>
-          <input className='border shadow p-1' id='username' name='username' type="text" />
+    <Card className='mt-4'>
+      <CardHeader>
+        <div className='flex justify-center w-full'>
+          <Image
+            src="/logo-bankme.png"
+            width={500}
+            height={500}
+            alt="Logo"
+            className='w-1/4'
+          />
         </div>
-        <div className='flex flex-col pb-1'>
-          <label htmlFor="">Password</label>
-          <input className='border shadow p-1' id='password' name='password' type="password" />
+        <CardTitle className='text-center mt-2'>Login</CardTitle>
+      </CardHeader>
+      <CardContent>
+        <form onSubmit={handleSubmit} className='flex flex-col justify-center w-full'>
+          <div className='flex flex-col pb-1'>
+            <Label className='mb-2' htmlFor="username">Username</Label>
+            <Input id='username' name='username' type="text" />
+          </div>
+          <div className='flex flex-col pb-1'>
+            <Label className='mb-2' htmlFor="password">Password</Label>
+            <Input id='password' name='password' type="password" />
+          </div>
+          <div className='flex flex-col justify-center pb-1 mt-2 mb-2'>
+            <Button className='bg-blue-700 hover:bg-blue-600' type="submit">Login</Button>
+          </div>
+      </form>
+      </CardContent>
+      <CardFooter>
+        {error && (
+        <div className='flex flex-col justify-center'>
+          {error.map((e, i) => (
+            <div key={i} className='text-red-400 bg-red-100 mb-2 p-2 rounded text-center'>{e}</div>
+          ))}
         </div>
-        <div className='flex flex-col justify-center pb-1 mt-2 mb-2'>
-          <button 
-          className="px-4 py-1 rounded-md bg-sky-500 hover:bg-sky-700 hover:text-cyan-50" 
-          type="submit">Login</button>
-        </div>
-    </form>
-    {error && (
-      <div className='flex flex-col justify-center'>
-        {error.map((e, i) => (
-          <div key={i} className='text-red-400 bg-red-100 mb-2 p-2 rounded text-center'>{e}</div>
-        ))}
-      </div>
-    )}
+      )}
+      </CardFooter>
+    </Card>
+    
+    
     </>
   )
 }
