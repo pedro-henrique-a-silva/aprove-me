a51aebaa814c937afae8a43ef08e93ddd4a8dccc
Pedro Henrique <pedro.henrique.a.s66@gmail.com>
2024-12-18 20:06:03 -0300

    feat: remove borda do contêiner na página de edição de recebível

--- web/src/app/payable/[id]/edit/page.tsx
+++ web/src/app/payable/[id]/edit/page.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 
 function page() {
   return (
-    <div className='container w-1/2 mx-auto mt-5 flex justify-center items-center border rounded border-cyan-100 shadow border-solid'>
+    <div className='container w-1/2 mx-auto mt-5 flex justify-center items-center'>
       <div className="flex flex-col  w-1/2">
         <CreatePayable isEditing={true}/>
       </div>
