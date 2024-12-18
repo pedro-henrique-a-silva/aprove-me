5b25d2a7165af1b42b804f2fecb71c1962751d67
Pedro Henrique <pedro.henrique.a.s66@gmail.com>
2024-12-18 20:04:48 -0300

    feat: substitui o cabeçalho por um menu de navegação estilizado com componentes do shadcn/ui

--- web/src/app/components/Header.tsx
+++ web/src/app/components/Header.tsx
@@ -1,16 +1,40 @@
+import { NavigationMenu, NavigationMenuItem, NavigationMenuList } from '@radix-ui/react-navigation-menu'
 import Link from 'next/link'
 import React from 'react'
+import Image from 'next/image'
+import { Button } from '@/components/ui/button'
+
 
 function Header() {
   return (
-    <div className='mx-auto flex justify-between bg-gray-400 p-6'>
-      <h1>Logo</h1>
-      <div className='flex justify-center items-center gap-2'>
-        <Link href={'/'}>Home</Link>
-        <Link href={'/payable'}>Recebiveis</Link>
-        <Link href={'/assignor'}>Cedente</Link>
-
-      </div>
+    <div className='mx-auto flex justify-between items-center bg-white p-2 shadow-sm shadow-slate-100 px-8'>
+        <Image
+          src="/logo-bankme.png"
+          width={50}
+          height={40}
+          alt="Logo"
+        />
+      <NavigationMenu>
+        <NavigationMenuList>
+          <NavigationMenuItem>
+            <Link href={'/'}>
+              <Button variant="link">
+                Home
+              </Button>
+            </Link>
+            <Link href={'/payable'}>
+              <Button variant="link">
+                  Recebíveis
+              </Button>
+            </Link>
+            <Link href={'/assignor'}>
+              <Button variant="link">
+                  Cedente
+              </Button>
+            </Link>
+        </NavigationMenuItem>
+      </NavigationMenuList>
+    </NavigationMenu>
     </div>
   )
 }
