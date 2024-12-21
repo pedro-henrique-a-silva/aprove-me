d618de77ac06a41baeade3b44ee51324e44eaa61
Pedro Henrique <pedro.henrique.a.s66@gmail.com>
2024-12-21 11:14:19 -0300

    feat: adiciona regras de ESLint específicas para componentes React

--- web/eslint.config.mjs
+++ web/eslint.config.mjs
@@ -11,6 +11,18 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+  {
+    "overrides": [
+      {
+        "files": ["./src/app/components/*.tsx"], 
+        "rules": {
+          "react-hooks/exhaustive-deps": "off", 
+          "@typescript-eslint/no-explicit-any": "off", 
+          "@typescript-eslint/no-unused-vars": "off", 
+        }
+      },
+    ]
+  }
 ];
 
 export default eslintConfig;
