11b37575fe99e6837c21169eb91ea5394f2b37b7
Pedro Henrique <pedro.henrique.a.s66@gmail.com>
2024-12-21 11:16:27 -0300

    refactor: atualiza a URL base da conexão com o servidor para utilizar variável de ambiente

--- web/src/app/utils/api-connection.ts
+++ web/src/app/utils/api-connection.ts
@@ -5,6 +5,8 @@ export const connection = axios.create({
   baseURL: BASE_URL,
 });
 
+
+const SERVER_BASE_URL = `${process.env.NEXT_SERVER_API_URL}/` || "http://localhost:3001/";
 export const serverConnection = axios.create({
-  baseURL: 'http://bankme_api:3001/',
+  baseURL: SERVER_BASE_URL,
 });
