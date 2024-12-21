416b048c68a7f3f3ddadf34de88ef2196fe8c5fb
Pedro Henrique <pedro.henrique.a.s66@gmail.com>
2024-12-21 11:38:49 -0300

    refactor: remove fragment desnecessário na página de pagamentos

--- web/src/app/payable/page.tsx
+++ web/src/app/payable/page.tsx
@@ -4,12 +4,10 @@ import RegisterPayableButton from '../components/RegisterPayableButton'
 
 function page() {
   return (
-    <>
-      <div className='p-6 flex justify-center items-center flex-col'>
+    <div className='p-6 flex justify-center items-center flex-col'>
         <RegisterPayableButton />
         <ListPayables/>
     </div>
-    </>
   )
 }
 
