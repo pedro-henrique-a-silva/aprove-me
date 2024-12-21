8765e85031947bd97c14213cb7ca5b5c70f96523
Pedro Henrique <pedro.henrique.a.s66@gmail.com>
2024-12-21 11:16:19 -0300

    refactor: ajusta a formatação do schema createPayable para melhorar a legibilidade

--- web/src/app/schemas/create-payable.ts
+++ web/src/app/schemas/create-payable.ts
@@ -3,12 +3,12 @@ import { z } from "zod";
 export const createPayableSchema = z.object({
   value: z
     .string({ message: "O valor não pode ser vazio." }),
-    emissionDate: z
-    .string({ message: "A data de Emissão não pode ser vazia." })
-    .regex(/^\d{4}-\d{2}-\d{2}$/, {
-      message: 'Data de emissão Inválida',
-    }),
-    assignors: z.string({ message: "O cedente não pode ser vazio." }).uuid(),
+  emissionDate: z
+  .string({ message: "A data de Emissão não pode ser vazia." })
+  .regex(/^\d{4}-\d{2}-\d{2}$/, {
+    message: 'Data de emissão Inválida',
+  }),
+  assignors: z.string({ message: "O cedente não pode ser vazio." }).uuid(),
 });
 
 export type CreatePayableData = z.infer<typeof createPayableSchema>;
